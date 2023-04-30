d7bc02a4e1ce0c7ecc84cd9aa4cf3c87a0cf1a34
ferrariflaviaa <flaviaferrarit@gmail.com>
2023-04-30 17:28:39 -0300

    feat: retrieving API data

--- Requisiçções HTTP com react/httpreact/src/App.js
+++ Requisiçções HTTP com react/httpreact/src/App.js
@@ -1,9 +1,32 @@
 import './App.css'
+import { useEffect, useState } from 'react'
+const url = 'http://localhost:3000/products'
 
 function App() {
+  const [products, setProducts] = useState([])
+
+  // 1 - resgatando dados
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(url)
+
+      const data = await res.json()
+
+      setProducts(data)
+    }
+    fetchData()
+  }, [])
+
   return (
-    <div className="app">
+    <div className="App">
       <h1>Lista de produtos</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            {product.name} - R$: {product.price}
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
